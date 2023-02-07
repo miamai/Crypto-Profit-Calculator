ddd354e08f62cfd6eb4e89e773aefd909fa1dc8a
miamai <cb121466@gmail.com>
2023-02-07 11:50:53 +0800

    introduction

---
+++ src/components/Home/About.jsx
@@ -0,0 +1,13 @@
+import { Typography } from '@mui/material';
+
+const About = () => {
+  return (
+    <div>
+      <Typography>技術</Typography>
+      <Typography>使用</Typography>
+      <Typography>構想</Typography>
+    </div>
+  );
+};
+
+export default About;
